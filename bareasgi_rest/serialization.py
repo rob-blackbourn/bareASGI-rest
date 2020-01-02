39f336dad0a5d7abf4ce10b747633f4578e049f0
Rob Blackbourn <rblackbourn@bhdgsystematic.com>
2020-01-02 16:51:31 +0000

    Enhanced date parsing

--- bareasgi_rest/serialization.py
+++ bareasgi_rest/serialization.py
@@ -8,27 +8,84 @@ import json
 import re
 from typing import (
     Any,
-    Dict
+    Callable,
+    Dict,
+    Optional,
+    Tuple
 )
 from urllib.parse import parse_qs
 
 DATETIME_ZULU_REGEX = re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$')
 
+DateTimeFormat = Tuple[str, re.Pattern, Optional[Callable[[str], str]]]
+
+DATETIME_FORMATS: Tuple[DateTimeFormat, ...] = (
+    (
+        "%Y-%m-%dT%H:%M:%SZ",
+        re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$'),
+        None
+    ),
+    (
+        "%Y-%m-%dT%H:%M:%S.%fZ",
+        re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z$'),
+        None
+    ),
+    (
+        "%Y-%m-%dT%H:%M:%S%z",
+        re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{2}:\d{2}$'),
+        lambda s: s[0:-3] + s[-2:]
+    ),
+    (
+        "%Y-%m-%dT%H:%M:%S.%f%z",
+        re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+[+-]\d{2}:\d{2}$'),
+        lambda s: s[0:-3] + s[-2:]
+    )
+)
+
+
+def json_to_datetime(value: str) -> Optional[datetime]:
+    if isinstance(value, str):
+        for fmt, pattern, transform in DATETIME_FORMATS:
+            if pattern.match(value):
+                s = transform(value) if transform else value
+                return datetime.strptime(s, fmt)
+    return None
 
 def as_datetime(dct):
     """Convert datetime like strings"""
     for key, value in dct.items():
-        if isinstance(value, str) and DATETIME_ZULU_REGEX.match(value):
-            dct[key] = datetime.fromisoformat(value[:-1])
+        if isinstance(value, str):
+            timestamp = json_to_datetime(value)
+            if timestamp:
+                dct[key] = timestamp
     return dct
 
+def datetime_to_json(timestamp: datetime) -> str:
+    if timestamp.tzinfo is None:
+        return "{year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}.{millis:02d}Z".format(
+            year=timestamp.year, month=timestamp.month, day=timestamp.day,
+            hour=timestamp.hour, minute=timestamp.minute, second=timestamp.second,
+            millis=timestamp.microsecond // 1000
+        )
+    else:
+        tz_seconds = timestamp.utcoffset().total_seconds()
+        tz_sign = '-' if tz_seconds < 0 else '+'
+        tz_minutes = int(abs(tz_seconds)) // 60
+        tz_hours = tz_minutes // 60
+        tz_minutes %= 60
+        return "{year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}.{millis:02d}{tz_sign}{tz_hours:02d}:{tz_minutes:02d}".format(
+            year=timestamp.year, month=timestamp.month, day=timestamp.day,
+            hour=timestamp.hour, minute=timestamp.minute, second=timestamp.second,
+            millis=timestamp.microsecond // 1000,
+            tz_sign=tz_sign, tz_hours=tz_hours, tz_minutes=tz_minutes
+        )
 
 class JSONEncoderEx(json.JSONEncoder):
     """Encode json"""
 
     def default(self, obj):  # pylint: disable=method-hidden,arguments-differ
         if isinstance(obj, datetime):
-            return obj.isoformat() + ('Z' if not obj.tzinfo else '')
+            return datetime_to_json(obj)
         elif isinstance(obj, Decimal):
             return float(
                 str(obj.quantize(Decimal(1))
--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -3,10 +3,24 @@
 from datetime import datetime
 import json
 
-from bareasgi_rest.serialization import JSONEncoderEx, as_datetime
+from bareasgi_rest.serialization import (
+    json_to_datetime,
+    datetime_to_json,
+    JSONEncoderEx,
+    as_datetime
+)
 
-def test_as_datetime():
+def test_json():
     """Test for as_datetime"""
+    for text in [
+        '2014-01-01T23:28:56.782Z',
+        '2014-02-01T09:28:56.321-10:00',
+        '2014-02-01T09:28:56.321+00:00'
+    ]:
+        timestamp = json_to_datetime(text)
+        roundtrip = datetime_to_json(timestamp)
+        assert text == roundtrip
+
     orig = {
         'one': 1,
         'timestamp': datetime(1967, 8, 12, 15, 42, 12),
