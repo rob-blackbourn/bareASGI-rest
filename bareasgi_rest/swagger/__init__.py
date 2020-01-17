d0576424008753c6065505132adc922266e44cda
Rob Blackbourn <rblackbourn@bhdgsystematic.com>
2020-01-17 09:02:16 +0000

    Removed unnecessary exports

--- bareasgi_rest/swagger/__init__.py
+++ bareasgi_rest/swagger/__init__.py
@@ -1,16 +1,12 @@
 """Swagger"""
 
 from .config import SwaggerConfig, SwaggerOauth2Config
-from .entry import make_swagger_entry
-from .paths import make_swagger_path
 from .repository import SwaggerRepository
 from .controller import SwaggerController
 
 __all__ = [
-    "make_swagger_entry",
-    "make_swagger_path",
+    "SwaggerController",
     "SwaggerRepository",
     "SwaggerConfig",
     "SwaggerOauth2Config",
-    "SwaggerController"
 ]
