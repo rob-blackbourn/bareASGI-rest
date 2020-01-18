1deb8ddd9eff20d15230124b6ec53b2f83d70cbf
Rob Blackbourn <rob.blackbourn@googlemail.com>
2020-01-18 08:05:10 +0000

    Tidy up

--- bareasgi_rest/swagger/errors.py
+++ bareasgi_rest/swagger/errors.py
@@ -6,7 +6,17 @@ from docstring_parser import DocstringRaises
 
 
 def gather_error_responses(docstring_raises: List[DocstringRaises]) -> Dict[int, Any]:
-    """Gather error responses"""
+    """Gather error responses
+
+    Looks for exceptions of type `HTTPError` with a description starting with
+    the error code: e.g. `"404, when a book is not found"`
+
+    Args:
+        docstring_raises (List[DocstringRaises]): The raises from the docstring
+
+    Returns:
+        Dict[int, Any]: The error response schema.
+    """
     responses: Dict[int, Any] = {}
     for raises in docstring_raises:
         if raises.type_name != 'HTTPError':
@@ -16,8 +26,9 @@ def gather_error_responses(docstring_raises: List[DocstringRaises]) -> Dict[int,
             continue
         try:
             error_code = int(first.strip())
+            description = rest.strip()
             responses[error_code] = {
-                'description': rest.strip()
+                'description': description
             }
         except:  # pylint: disable=bare-except
             continue
