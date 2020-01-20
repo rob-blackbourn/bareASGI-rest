eb218e86d984bde2a2b690ec18fb8443efb3b1f0
Rob Blackbourn <rblackbourn@bhdgsystematic.com>
2020-01-20 07:30:29 +0000

    Added datetime tests

--- tests/protocol/iso_8601/test_date_time.py
+++ tests/protocol/iso_8601/test_date_time.py
@@ -1,12 +1,14 @@
 """Tests for JSON serialization"""
 
+from datetime import timezone, timedelta
+
 from bareasgi_rest.protocol.iso_8601 import (
     iso_8601_to_datetime,
     datetime_to_iso_8601
 )
 
 
-def test_json():
+def test_roundtrip():
     """Test for json_to_python"""
     for text in [
             '2014-01-01T23:28:56.782Z',
@@ -16,3 +18,31 @@ def test_json():
         timestamp = iso_8601_to_datetime(text)
         roundtrip = datetime_to_iso_8601(timestamp)
         assert text == roundtrip
+
+
+def test_zulu_tz_winter():
+    """Test for the zulu timezone"""
+    timestamp = iso_8601_to_datetime('2014-01-01T23:28:56.782Z')
+    assert timestamp.timetuple() == (2014, 1, 1, 23, 28, 56, 2, 1, -1)
+    assert timestamp.tzinfo is None
+
+
+def test_zulu_tz_summer():
+    """Test for the zulu timezone"""
+    timestamp = iso_8601_to_datetime('2014-08-01T23:28:56.782Z')
+    assert timestamp.timetuple() == (2014, 8, 1, 23, 28, 56, 4, 213, -1)
+    assert timestamp.tzinfo is None
+
+
+def test_tz_offset():
+    """Test for timezone offset"""
+    timestamp = iso_8601_to_datetime('2014-02-01T09:28:56.321-10:00')
+    assert timestamp.timetuple() == (2014, 2, 1, 9, 28, 56, 5, 32, -1)
+    assert timestamp.tzinfo == timezone(timedelta(hours=-10))
+
+
+def test_tz_offset_0():
+    """Test for timezone offset"""
+    timestamp = iso_8601_to_datetime('2014-02-01T09:28:56.321+00:00')
+    assert timestamp.timetuple() == (2014, 2, 1, 9, 28, 56, 5, 32, -1)
+    assert timestamp.tzinfo == timezone(timedelta(hours=0))
